9a8d3dd0ffc599ec25c19c5b17732f6ce6d67b44
Hitamuki <hitamuki024@gmail.com>
2024-07-07 03:59:47 +0000

    ビューの追加

--- src/BookmarksManager/Controllers/HelloWorldController.cs
+++ src/BookmarksManager/Controllers/HelloWorldController.cs
@@ -5,14 +5,16 @@ namespace BookmarksManager.Controllers;
 public class HelloWorldController : Controller
 {
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int numTimes = 1)
+    // GET: /HelloWorld/Welcome/
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        ViewData["Message"] = "Hello" + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
